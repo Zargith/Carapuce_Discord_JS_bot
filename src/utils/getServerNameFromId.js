d8fbfbcb42437bff2b8b6e1b00e75b33827c5884
Zargith <stanislas.hegron@gmail.com>
2021-03-24 16:34:50 +0900

    FIX: Fixed === error when getting name of a server

--- src/utils/getServerNameFromId.js
+++ src/utils/getServerNameFromId.js
@@ -2,6 +2,6 @@ module.exports = function(serverId) {
 	const servers = bot.guilds.cache.array();
 
 	for (let i = 0; i < servers.length; i++)
-		if (servers[i].id === serverId)
+		if (servers[i].id == serverId)
 			return (servers[i].name);
 };
